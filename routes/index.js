da106368d965548fbb2edb263d3f046362d0a132
Rowan <rowan.tate@gmail.com>
2014-04-13 22:59:36 +1200

    logging removal

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 exports.index = function(req, res){
 	var Game = mongoose.model('Game');
 	Game.find({hasMapping: true}).sort({ fullName: 1 }).exec(function(err, games){
-		console.log(games);
+		
 		res.render('index', 
 		{ 
 			title: 'Select file to upload',
