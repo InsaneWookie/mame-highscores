a80659f0670ebdd96768c3c9cd80f804d62670be
Rowan <rowan.tate@gmail.com>
2014-08-02 11:45:58 +1200

    Changed for open shift deplyment

---
+++ api/controllers/GamePlayedController.js
@@ -0,0 +1,11 @@
+/**
+ * GamePlayedController
+ *
+ * @description :: Server-side logic for managing Gameplayeds
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
