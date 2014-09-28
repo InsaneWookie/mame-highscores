aa50ffec2ddcca15d3cc952eaefec33bb900ca34
Rowan <rowan.tate@gmail.com>
2014-09-28 16:03:51 +1300

    Added rank populate query

--- migrations/20140928_1130_storing_score_rank.sql
+++ migrations/20140928_1130_storing_score_rank.sql
@@ -1,1 +1,9 @@
 ALTER TABLE score ADD COLUMN rank INTEGER;
+
+UPDATE score s SET rank = r.rank
+FROM (SELECT id, rank()
+      OVER (PARTITION BY game_id
+      ORDER BY (0 || regexp_replace(score, E'[^0-9]+','','g'))::bigint DESC ) as rank
+      FROM
+      score ) r
+WHERE s.id = r.id;
